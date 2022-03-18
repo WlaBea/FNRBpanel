e001c5a7b663f4746683c3f839c369bb4036fa97
WlaBea <92812560+WlaBea@users.noreply.github.com>
2022-03-18 12:05:01 +0100

    Login page + navbar

--- js/main.js
+++ js/main.js
@@ -1,14 +1,7 @@
 const hamburger = document.querySelector('.hamburger');
 const menu = document.querySelector('.menu');
-const login = document.getElementById("login_button");
 
 hamburger.addEventListener('click', function() {
     this.classList.toggle('is_active');
-    menu.classList.toggle('menuhamb');
-});
-
-login.addEventListener('click', function() {
-    document.getElementById("login_page_id").style.display = "none";
-    document.getElementById("nav_id").style.display = "flex";
-    document.getElementById("main_page_id").style.display = "grid";
+    menu.classList.toggle('show');
 });
